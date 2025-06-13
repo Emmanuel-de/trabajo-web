f23ae664059f4fc7110a38ceda2def713191406e
Emmanuel-de <rockb1517@gmail.com>
2025-06-12 23:09:03 -0600

    agrege-blade

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage'); // Esto cargará resources/views/homepage.blade.php
 });
+
+Route::get('/registro', function () {
+    return view('registro');
+})->name('registro.form'); // Dale un nombre para facilitar la referencia
